b7d7517dfa7217bb811c1b7d56307c955540a5bd
MAY <mysterious.may.96@gmail.com>
2022-03-28 15:45:42 +0500

    ...

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -291,7 +291,7 @@ class Functions
                             $consistent_in[$debitor][1] += 1;
                             $consistent_in[$debitor][2] += 1;
                             $consistent_in[$debitor][3] += (float)$cin_transaction->transaction_amount;
-                            $consistent_in[$debitor][4] = [$cin_transaction];
+                            $consistent_in[$debitor][4] = [1,$cin_transaction];
                         }
                     }
                     else{
@@ -338,7 +338,7 @@ class Functions
                             $consistent_out[$creditor][1] += 1;
                             $consistent_out[$creditor][2] += 1;
                             $consistent_out[$creditor][3] += (float)$cout_transaction->transaction_amount;
-                            $consistent_out[$creditor][4] = [$cout_transaction];
+                            $consistent_out[$creditor][4] = [1,$cout_transaction];
                         }
                     }
                     else{
