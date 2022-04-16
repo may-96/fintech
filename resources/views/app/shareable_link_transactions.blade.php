68b91b19c267f21aa9e32411b74061ebf4a4a35a
MAY <mysterious.may.96@gmail.com>
2022-04-16 06:36:54 +0500

    ...

--- resources/views/app/shareable_link_transactions.blade.php
+++ resources/views/app/shareable_link_transactions.blade.php
@@ -120,7 +120,7 @@
 </style>
 @endsection
 @section('header')
-    <section class="wrapper vh-100 d-flex align-items-center hero_section_bg" style="background-image: url({{asset('images/background/Hexagon.svg')}})">
+    <section class="wrapper vh-100 d-flex align-items-center hero_section_bg pt-16" style="">
         <div class="container text-center">
             <div class="row">
                 <div class="col-12">
