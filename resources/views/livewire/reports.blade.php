3a77f3c9b8cb3479d41eb8ba13c0267c3df1cbe6
MAY <mysterious.may.96@gmail.com>
2022-04-20 20:55:01 +0500

    ...

--- resources/views/livewire/reports.blade.php
+++ resources/views/livewire/reports.blade.php
@@ -576,8 +576,10 @@
                     </div>
                     <div class="col-6">
                         @if ($data[0] == 'self' || (($data[0] == 'shared' || $data[0] == 'self_shared' || $data[0] == 'link_shared') && $access['view_credit_score'] == 1))
-                            <p class="m-0 text-end"><span class="fw-bold">Credit Score:</span> <span class="text-muted fs-20">{{ round($report_data[8], 0) }}</span></p>
+                            <p class="m-0 text-end"><span class="fw-bold">Cash Flow Score:</span> <span class="text-muted fs-20">{{ round($report_data[8], 0) }}</span></p>
                             <p class="text-end fw-bold text-{{ $report_data[10] }}">{{ $report_data[9] }}</p>
+                            @if($report_data[18] > 0)<p class="m-0 text-end"><span class="fw-bold">Credit Score:</span> <span class="text-muted fs-20">{{ round($report_data[18], 1) }}</span></p>
+                            <p class="text-end fw-bold text-{{ $report_data[20] }}">{{ $report_data[19] }}</p>@endif
                         @endif
                         @if ($report_data[11] > 0)
                             <p class="m-0 text-end"><span class="">Saving per Month:</span> <span>{{ $report_data[16] . ' ' . round($report_data[11], 2) }}</span></p>
