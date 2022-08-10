2bc5fff6f871ba2dd05ae3f7fa06f353323aa0fe
MAY <mysterious.may.96@gmail.com>
2022-08-11 03:19:16 +0500

    ...

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -140,7 +140,7 @@ class Functions
                     "category_id" => isset($t["categorisation"]) ? ( $transaction_type == 'debit' ? $t["categorisation"]["categoryId"] : (int)$t["categorisation"]["categoryId"] + 100) : null,
 
                     "merchant_name" => isset($t["cleaning"]) ? $t["cleaning"]["merchantName"] : null,
-                    "transaction_type" => isset($t["cleaning"]) ? $t["cleaning"]["transactionType"] : null,
+                    "transaction_type" => isset($t["cleaning"]) ? $t["cleaning"]["transactionCode"] : null,
                     "merchant_category_code" => (isset($t["cleaning"]) && isset($t["cleaning"]["merchantCategoryCode"])) ? $t["cleaning"]["merchantCategoryCode"] : null,
                     "invoice_number" => (isset($t["cleaning"]) && isset($t["cleaning"]["invoiceNumber"])) ? $t["cleaning"]["invoiceNumber"] : null,
 
